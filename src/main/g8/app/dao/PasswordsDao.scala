f94e2762d58fcc59f9274f1082053dd660185d9a
setrar <setrar@valiha.com>
2019-02-19 22:37:42 -0500

    Update PasswordsDao.scala

--- src/main/g8/app/dao/PasswordsDao.scala
+++ src/main/g8/app/dao/PasswordsDao.scala
@@ -15,16 +15,15 @@ trait PasswordComponent { self: HasDatabaseConfigProvider[JdbcProfile] =>
   class PasswordTable(tag: Tag) extends Table[Password](tag, "PASSWORDS") {
 
     // scalastyle:off magic.number
-    def providerKey: Rep[String] =
-      column[String]("providerKey", O.Length(45, varying = true), O.PrimaryKey)
-
-
+    def providerKey: Rep[String] = column[String]("providerKey", O.Length(45, varying = true), O.PrimaryKey)
     def hasher: Rep[String] = column[String]("hasher")
     def password: Rep[String] = column[String]("password")
     def salt: Rep[String] = column[String]("salt")
+    // scalastyle:on magic.number
+
     // scalastyle:off method.name
     override def * : ProvenShape[Password] =
-      (providerKey, hasher, password, salt) <> (Password.tupled, Password.unapply)
+      (providerKey ?, hasher, password, salt) <> (Password.tupled, Password.unapply)
     // scalastyle:on method.name
 
   }
