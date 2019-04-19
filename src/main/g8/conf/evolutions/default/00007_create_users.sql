2748f4e4866fb5c2bb076fbfb898f8126dd93157
setrar <setrar@valiha.com>
2019-04-19 18:32:14 -0400

    Update 00007_create_users.sql

--- src/main/g8/conf/evolutions/default/00007_create_users.sql
+++ src/main/g8/conf/evolutions/default/00007_create_users.sql
@@ -2,14 +2,15 @@
 
 # --- !Ups
 
+
 CREATE TABLE `USERS` (
   `user` BIGINT(20) NOT NULL,
   `number` VARCHAR(45) NULL,
   `providerKey` VARCHAR(45) NULL,
   `active` TINYINT(1) NULL,
-  `created` TIMESTAMP NULL
+  `created` TIMESTAMP NULL,
   PRIMARY KEY (`user`),
-  INDEX `fk_USERS_LOGINS1_idx` (`providerKey` ASC) VISIBLE,
+  INDEX `fk_USERS_LOGINS1_idx` (`providerKey` ASC) ,
   CONSTRAINT `fk_USERS_LOGINS1`
     FOREIGN KEY (`providerKey`)
     REFERENCES `LOGINS` (`providerKey`)
